895bdc5126a84ed41e1a6126b2508163848b7719
refresh2021 <irtijanafis@gmail.com>
2020-12-16 19:10:37 -0700

    moving address

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navibar  from './Components/Navibar'
 import Jumbotron from './Components/Jumbotron'
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route
 } from 'react-router-dom'

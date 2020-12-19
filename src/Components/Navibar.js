ecee477575ffb0e8ef4c613c0ea951c62b996c9c
refresh2021 <irtijanafis@gmail.com>
2020-12-18 21:18:37 -0700

    Fixed issues with menu.

--- src/Components/Navibar.js
+++ src/Components/Navibar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
